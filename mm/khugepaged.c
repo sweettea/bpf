176048a92c7b90364523d9bf5e427b4c2f45e5c1
Sweet Tea Dorminy <sweettea-kernel@dorminy.me>
2024-12-12 11:53:12 -0500

    mm/khugepaged: fix a tracepoint in error cases
    TODO
    
    Signed-off-by: Sweet Tea Dorminy <sweettea-kernel@dorminy.me>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1432,8 +1432,9 @@ out_unmap:
 		*mmap_locked = false;
 	}
 out:
-	trace_mm_khugepaged_scan_pmd(mm, &folio->page, writable, referenced,
-				     none_or_zero, result, unmapped);
+	trace_mm_khugepaged_scan_pmd(mm, folio ? &folio->page : NULL,
+				     writable, referenced, none_or_zero,
+				     result, unmapped);
 	return result;
 }
 
